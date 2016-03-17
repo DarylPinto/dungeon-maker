b1d900d003d19d4355c7616a5e09cdc7dafe3bb4
DarylPinto <Daryl@D20tower.scratch.local>
2016-03-17 11:18:04 -0400

    Compartmentalize and cleanup GUI bookmarklet

--- monster-scrape-bookmarklet/resources/js/gui-scrape.js
+++ monster-scrape-bookmarklet/resources/js/gui-scrape.js
@@ -1,87 +1,15 @@
-/***************************************
-
-Bookmarklet for collecting monster data with a GUI to add biomes of residence
-
-Yes I know it's coded like garbage.
-
-https://docs.google.com/document/d/1d4z_ztfSXV8ZtQddIXO0RVZqA2bN1t3s0LWCU50HzAA/edit?usp=sharing_eid&ts=56e8c6af
-
-***************************************/
-
 (function(){
 
-///////////////////
-//Add materialize//
-///////////////////
-
+//Add materialize
 $('head').append('<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">');
 $('head').append('<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.css"  media="screen,projection"/>');
 $('head').append('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>');
-$('head').append('<style type="text/css">body div{font-family: "Roboto", sans-serif;}[type="radio"]:not(:checked) + label:before{border: 2px solid transparent}</style>')
+
 /////////////////////
-//Collect Page Info//
+//Utility functions//
 /////////////////////
 
-biomes = {
-		
-		basic: [
-			'Classic',
-			'Earth',
-			'Air',
-			'Fire',
-			'Water',
-			'Light',
-			'Shadow',
-			'Darkness',
-			'Forrested',
-			'Plains',
-			'Beach',
-			'Metallic',
-			'Electric',
-			'Mountains',
-			'Red',
-			'Orange',
-			'Yellow',
-			'Green',
-			'Blue',
-			'Violet',
-			'Sonic',
-			'Acidic',
-			'Frosted',
-			'Insulated',
-			'Masterwork'
-		],
-
-		advanced: [
-			'Pixel',
-			'Demonic',
-			'Void',
-			'Angelic',
-			'Eldritch',
-			'Chaotic',
-			'Orderly',
-			'Plasma',
-			'Industrial',
-			'Clockwork',
-			'Temporal',
-			'Spatial',
-			'Dank',
-			'Automotive',
-			'Stellar',
-			'Lunar',
-			'Vicordian',
-			'Etheral',
-			'Astral',
-			'Bouncing',
-			'Gilded',
-			'Draconic',
-			'Fairy',
-			'Urban',
-			'Copyright'
-		]
-
-	}
-
+//Monster Object constructor
 function Monster(name, xp, residing_biomes, url_snippet){
 	this.name = name;
 	this.xp = xp;
@@ -89,12 +17,13 @@ function Monster(name, xp, residing_biomes, url_snippet){
 	this.url_snippet = url_snippet;
 }
 
-var monsters = [];
-
+//Remove base URL and content after #
 function stripURL(str){
 	str = str.replace('http://paizo.com/pathfinderRPG/prd/', '');
 	return str.slice(0, str.indexOf('#'));
 }
+
+//Get Monster Object via name
 function getMonsterByName(name){
 	var monster = null;
 	monsters.forEach(function(m){
@@ -102,16 +31,18 @@ function getMonsterByName(name){
 	});
 	return monster;
 }
+
+//Custom array to string function
 function stringifyArr(arr){
 	var str = "[";
 	arr.forEach(function(e){
-		str += "'"+e+"'";
-		str += ", ";
+		str += "'"+e+"', ";
 	});
-	str += "]"
-	str = str.replace("', ]", "']");
-	return str;
+	str += "]";
+	return str.replace("', ]", "']");	
 }
+
+//Log monster object string to textarea
 function logMonsters(){
 	var output_str = '';
 	monsters.forEach(function(m){
@@ -119,6 +50,34 @@ function logMonsters(){
 	});
 	$('#string-output').val(output_str);
 }
+
+////////
+//Main//
+////////
+
+var html = [
+'<main>',
+'	<div id="select-monster"></div>',
+'	<div id="select-biomes" class="row">',
+'		<div id="basic" class="col s6">',
+'			<h5>Basic Biomes</h5>',
+'		</div>',
+'		<div id="advanced" class="col s6">',
+'			<h5>Advanced Biomes</h5>',
+'		</div>',
+'	</div>',
+'	<textarea id="string-output" class="tooltipped" data-position="top" data-tooltip="Click to highlight, Ctrl+C to copy!" data-delay="50"></textarea>',
+'</main>'
+].join('');
+
+var biomes = {	
+	basic: ['Acidic', 'Air', 'Beach', 'Blue', 'Classic', 'Darkness', 'Earth', 'Electric', 'Fire', 'Forrested', 'Frosted', 'Green', 'Insulated', 'Light', 'Masterwork', 'Metallic', 'Mountains', 'Orange', 'Plains', 'Red', 'Shadow', 'Sonic', 'Violet', 'Water', 'Yellow'],
+	advanced: ['Angelic', 'Astral', 'Automotive', 'Bouncing', 'Chaotic', 'Clockwork', 'Copyright', 'Dank', 'Demonic', 'Draconic', 'Eldritch', 'Etheral', 'Fairy', 'Gilded', 'Industrial', 'Lunar', 'Orderly', 'Pixel', 'Plasma', 'Spatial', 'Stellar', 'Temporal', 'Urban', 'Vicordian', 'Void']
+}
+
+var monsters = [];
+
+//Gather Monster info to store in `monsters` array
 $('.stat-block-title').each(function(){
 
 	var m = new Monster();
@@ -139,95 +98,29 @@ $('.stat-block-title').each(function(){
 	if(m.xp.length > 0) monsters.push(m);
 });
 
-/////////////
-//Build GUI//
-/////////////
+//Build GUI
+$('body').empty().append(html);
 
-var main = document.createElement('main');
-
-$(main).append('<div id="select-monster"></div>');
-$(main).append('<div id="select-biomes" class="row"></div>');
-$(main).append('<textarea id="string-output"></textarea>');
-
-$('body').empty().append(main);
-
-$(main).css({
-	'position' : 'relative',
-	'height' : '100vh',
-	'width' : '100vw',
-	'font-family' : '"Roboto", sans-serif',
-	'background-color' : 'white'
-});
-
-$('#select-monster').css({
-	'position' : 'absolute',
-	'overflow' : 'auto',
-	'width' : '60vw',
-	'height' : '65vh',
-	'padding' : '35px',
-	'left' : '0',
-	'top' : '0'
-});
-
-$('#select-biomes').css({
-	'position' : 'absolute',
-	'overflow' : 'auto',
-	'width' : '40vw',
-	'height' : '100vh',
-	'background-color' : 'white',
-	'z-index' : '10',
-	'right' : '0',
-	'top' : '0',
-	'box-shadow' : '0 0 10px rgba(0,0,0,0.3)'
-});
-
-$('#string-output').css({
-	'position' : 'absolute',
-	'padding' : '20px',
-	'width' : '60vw',
-	'height' : '35vh',
-	'left' : '0',
-	'bottom' : '0',
-	'border' : 'none',
-	'font-family' : 'monospace',
-	'cursor' : 'pointer',
-	'box-shadow' : '0 0 10px rgba(0,0,0,0.3)'
-}).addClass('tooltipped').attr({
-	'data-position' : 'top',
-	'data-tooltip' : 'Click to highlight, then Ctrl+C to copy!',
-	'data-delay' : '50'
-});
-
+//Add monsters to GUI
 monsters.forEach(function(m){
 	$('#select-monster').append('<p><input name="monster" type="radio" id="'+m.name+'" /><label for="'+m.name+'">'+m.name+'</label></p>');
 });
 
-$('#select-biomes').append('<div id="basic" class="col s6"></div>').append('<div id="advanced" class="col s6"></div>');
-
-$('#select-biomes #basic').append('<h5>Basic Biomes</h5>');
-
+//Add biomes to GUI
 biomes.basic.forEach(function(b){
 	$('#select-biomes #basic').append('<p><input type="checkbox" class="filled-in" id="'+b+'" /><label for="'+b+'">'+b+'</label></p>');
 });
-
-$('#select-biomes #advanced').append('<h5>Advanced Biomes</h5>');
-
 biomes.advanced.forEach(function(b){
 	$('#select-biomes #advanced').append('<p><input type="checkbox" class="filled-in" id="'+b+'" /><label for="'+b+'">'+b+'</label></p>');
 });
 
-$('#select-biomes p').css({
-	'margin-top' : '0',
-	'margin-bottom' : '3px',
-	'margin-left' : '20px'
-});
+//Select First Monster from GUI
+$('#select-monster p:first-child input').prop('checked', true);
 
-$('#select-biomes h5').css({'margin-left': '20px'});
-
-$('#select-monster p:first-child input').prop( "checked", true );
-
+//Log monster object(s) to textarea
 logMonsters();
 
+//When a monster is selected, select appropriate biome checkboxes
 $('#select-monster input').change(function(){
 	$('#select-biomes input').prop('checked', false);
 	var monstername = $('#select-monster input:checked').attr('id');
@@ -236,6 +129,7 @@ $('#select-monster input').change(function(){
 	});
 });
 
+//When biome is selected, update current monster's `residing_biomes` and update textarea
 $('#select-biomes input').change(function(){
 	var biomes = [];
 	$('#select-biomes input:checked').each(function(){
@@ -246,10 +140,9 @@ $('#select-biomes input').change(function(){
 	logMonsters();
 });
 
+//When text area is clicked, select it
 $('#string-output').click(function(){
 	$(this).select();
 })
-
-
 
 })();
